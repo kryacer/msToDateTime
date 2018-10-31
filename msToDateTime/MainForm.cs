30e3873215e6d1ce253e6b8e6d56f33322a4a525
Askhat Tuyenbayev <tm.askhat@gmail.com>
2018-10-31 18:29:13 +0600

    refactoring2

--- msToDateTime/MainForm.cs
+++ msToDateTime/MainForm.cs
@@ -1,11 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace msToDateTime
@@ -16,7 +9,10 @@ namespace msToDateTime
         {
             InitializeComponent();
         }
-        
+        private bool IsLeapYear(int year)
+        {
+            return (year % 4 == 0 && year % 100 != 0) || year % 400 == 0;
+        }
         private void btnStart_Click(object sender, EventArgs e)
         {
             try
@@ -24,80 +20,47 @@ namespace msToDateTime
                 //long msec = Convert.ToInt64(InputBox.Text);
                 long msec = (long)(dateTimePicker1.Value - new DateTime(1970, 1, 1)).TotalMilliseconds;
                 long sec = msec / 1000;
-                    long msecRest = msec % 1000;
+                long msecRest = msec % 1000;
                 long min = sec / 60;
-                    long secRest = sec % 60;
+                long secRest = sec % 60;
                 long hour = min / 60;
-                    long minRest = min % 60;
+                long minRest = min % 60;
                 long days = (hour / 24);
-                    long hourRest = hour % 24;
+                long hourRest = hour % 24;
 
                 long staticDays = days;
+                int currentYear = 1970;
 
-                int yearCounter = 0;
-                
                 for (int i = 1970; i < 1970 + (staticDays / 364); i++)
                 {
-                    if ((i % 4 == 0 && i % 100 != 0) || i % 400 == 0) //Високосный 366
+                    int minDaysInYear = 365;
+                    if (IsLeapYear(i)) //Високосный 366
+                        minDaysInYear = 366;
+                    if (days >= minDaysInYear)
                     {
-                        if (days > 365)
-                        {
-                            days -= 366;
-                            yearCounter++;
-                        }
-                        else { break; }
+                        days -= minDaysInYear;
+                        currentYear = i + 1;
                     }
-                    if ((i % 100 == 0 && i % 400 != 0) || i % 4 != 0) //Не високосный 365
-                    {
-                        if (days > 364)
-                        {
-                            days -= 365;
-                            yearCounter++;
-                        }
-                        else { break; }
-                    }
+                    else { break; }
                 }
-                int currentYear = 1970 + yearCounter;
+
                 int monthCounter = 1;
                 bool leapYear = false;
-                if ((currentYear % 4 == 0 && currentYear % 100 != 0) || currentYear % 400 == 0)
+                int[] monthDays = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+                if (IsLeapYear(currentYear))
                 {
                     leapYear = true;
+                    monthDays[1] = 29;
                 }
-                if ((currentYear % 100 == 0 && currentYear % 400 != 0) || currentYear % 4 != 0)
-                {
-                    leapYear = false;
-                }
-                int[] monthDays = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+
                 for (int i = 0; i < 12; i++)
                 {
-                    if (leapYear == true)
+                    if (days - monthDays[i] >= 0)
                     {
-                        if (days - monthDays[i] > 0)
-                        {
-                            if (i == 1)
-                            {
-                                days -= 29;
-                                monthCounter++;
-                            }
-                            else
-                            {
-                                days -= monthDays[i];
-                                monthCounter++;
-                            }
-
-                        }
-                        else { break; }
+                        days -= monthDays[i];
+                        monthCounter++;
                     }
-                    else
-                    {
-                        if (days - monthDays[i] > 0)
-                        {
-                            days -= monthDays[i];
-                            monthCounter++;
-                        }
-                        else { break; }
-                    }
+                    else { break; }
                 }
                 long currentDay = 1 + days;
 
@@ -105,7 +68,7 @@ namespace msToDateTime
 
                 TimeBox.Text = hourRest.ToString() + ":" + minRest.ToString() + ":" + secRest.ToString() + ":" + msecRest.ToString();
 
-                
+
                 CorrectOutputBox.Text = new DateTime(1970, 1, 1).AddMilliseconds(msec).ToString("dd.MM.yyyy HH:mm:ss:fff");
             }
             catch (FormatException ex) { MessageBox.Show(ex.Message); }
